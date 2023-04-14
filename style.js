c9de18ae62f024ac62626b64ef282ace3ce8264f
lucasbertolinogb <102763228+lucasbertolinogb@users.noreply.github.com>
2023-04-14 10:48:51 -0300

    Create style.js

---
+++ style.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    texto: {
+        fontSize: '30px',
+        color: 'White',
+        fontWeight: 'bold'
+    }
+})
+
+export default styles;
